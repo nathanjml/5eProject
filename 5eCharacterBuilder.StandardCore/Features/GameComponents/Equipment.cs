6c2b0354276022d2bbe1272ceb438a6e0a7a819c
Nathan J. Murphy <nathan.murphy@selu.edu>
2018-08-30 16:45:16 -0700

    Running application. Added some Game Component Entities

---
+++ 5eCharacterBuilder.StandardCore/Features/GameComponents/Equipment.cs
@@ -0,0 +1,17 @@
+﻿using _5eCharacterBuilder.StandardCore.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5eCharacterBuilder.StandardCore.Features.GameComponents
+{
+    public class Equipment : IEntity
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsCustom { get; set; }
+    }
+}
---
+++ 5eCharacterBuilder.StandardCore/Features/GameComponents/Feat.cs
@@ -0,0 +1,16 @@
+﻿using _5eCharacterBuilder.StandardCore.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5eCharacterBuilder.StandardCore.Features.GameComponents
+{
+    public class Feat : IEntity
+    {
+        public int Id { get; set; }
+        public string FeatName { get; set; }
+        public string FeatDescription { get; set; }
+        public int RulebookPageRef { get; set; }
+        public bool IsCustom { get; set; }
+    }
+}
