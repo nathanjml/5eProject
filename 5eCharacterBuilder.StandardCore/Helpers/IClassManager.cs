c48eaba9eed27f7863bec97426076cfc87bdd439
Nathan J. Murphy <nathan.murphy@selu.edu>
2018-08-30 17:11:55 -0700

    Added starting point for Schmidt with Class Manager

---
+++ 5eCharacterBuilder.StandardCore/Helpers/IClassManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5eCharacterBuilder.StandardCore.Helpers
+{
+    public interface IClassManager
+    {
+        void IncreaseLevel(int amountBy = 1);
+        void DecreaseLevel(int amountBy = 1);
+        void ChangeClass(string targetClass);
+    }
+}
